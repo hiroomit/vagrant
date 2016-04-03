e38d2f8d8b83f1fbedd12fcffca8f2799be62885
Hiroomi Taniguchi <hiroomi.taniguchi@gmail.com>
2016-04-03 23:48:50 +0900

    set to default network

--- provision_fix.sh
+++ provision_fix.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-docker network rm my-bridge-network
-docker network create -d bridge --subnet=172.18.0.0/16 my-bridge-network
-
+#docker network rm my-bridge-network
+#docker network create -d bridge --subnet=172.18.0.0/16 -o com.docker.network.bridge.enable_icc=true -o com.docker.network.bridge.enable_ip_masquerade=true -o com.docker.network.bridge.host_binding_ipv4=0.0.0.0 -o com.docker.network.bridge.name=docker1 -o com.docker.network.driver.mtu=1500 my-bridge-network
